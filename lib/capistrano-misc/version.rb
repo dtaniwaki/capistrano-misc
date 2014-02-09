a3ad11348b401c50960aee82ced890188a0e39a3
dtaniwaki <daisuketaniwaki@gmail.com>
2014-02-09 21:42:29 +0900

    Bump version to 0.0.4

--- lib/capistrano-misc/version.rb
+++ lib/capistrano-misc/version.rb
@@ -2,7 +2,7 @@ module CapistranoMisc
   class Version
     MAJOR = 0 unless defined? CapistranoMisc::Version::MAJOR
     MINOR = 0 unless defined? CapistranoMisc::Version::MINOR
-    PATCH = 2 unless defined? CapistranoMisc::Version::PATCH
+    PATCH = 4 unless defined? CapistranoMisc::Version::PATCH
     PRE = nil unless defined? CapistranoMisc::Version::PRE
 
     class << self
